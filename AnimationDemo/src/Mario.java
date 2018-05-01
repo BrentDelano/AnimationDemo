1c005efb053920734f4515f4edf17a1c73b5d9e1
mkhosla770 <mkhosla770@PR04.hhs.win.fuhsd.org>
2018-05-01 11:35:15 -0700

    changed act

--- AnimationDemo/src/Mario.java
+++ AnimationDemo/src/Mario.java
@@ -10,6 +10,7 @@ public class Mario extends Sprite {
 	public static final int MARIO_WIDTH = 40;
 	public static final int MARIO_HEIGHT = 60;
 
+
 	public Mario(PImage img, int x, int y) {
 		super(img, x, y, MARIO_WIDTH, MARIO_HEIGHT);
 	}
@@ -25,6 +26,18 @@ public class Mario extends Sprite {
 
 	public void act(ArrayList<Shape> obstacles) {
 		// FALL (and stop when a platform is hit)
+		boolean isFalling = true;
+		for(Shape s: obstacles)
+		{
+			if(s.intersects(this)) {
+				isFalling = false;
+			}
+		}
+		if(isFalling)
+		{
+			super.moveByAmount(0, 1);
+		}
+		
 	}
 
 
